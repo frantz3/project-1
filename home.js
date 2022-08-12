2be74ee68a0dd4d909cacc25b3bcaa3e91eba317
frantzmoseau <frantzmoseau35@gmail.com>
2022-08-11 20:05:26 -0400

    update

---
+++ home.js
@@ -0,0 +1,43 @@
+const highlight = document.querySelector('#header')
+const content = document.querySelector('#content')
+
+const h1 = document.createElement('h1')
+const h12 = document.createElement('h1')
+
+h1.textContent = 'Header'
+h12.textContent = 'Content'
+
+highlight.append(h1)
+content.append(h12)
+
+function createHomeHighlight () {
+
+        fetch(api_url)
+        .then( (r) => r.json())
+        .then((data) => getHomeData(data))
+}
+
+function getHomeData(characterData) {
+    console.log(characterData)
+    characterData.forEach( character => makeOneCard(character))
+}
+
+function makeOneCard(character) {
+    console.log(character)
+
+    const div = document.createElement('div')
+    div.className = 'card'
+    content.append(div)
+
+    const name = document.createElement('h2')
+    name.innerText = character.name
+    div.append(h2)
+
+    
+    const img = document.createElement('img')
+    img.src = character.img
+    img.alt = character.img
+    div.append(img)
+}
+
+createHomeHighlight()
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-fetch('https://www.breakingbadapi.com/api/characters')
-.then( (r) => r.json())
-.then((data) => console.log(data))
+// const api_url = 'https://www.breakingbadapi.com/api/characters'
+
+// fetch('https://www.breakingbadapi.com/api/characters')
+// .then( (r) => r.json())
+// .then((data) => console.log(data))
+
+const home = document.querySelectorAll('a')[0]
+const characterInfo = document.querySelectorAll('a')[1]
+
+home.addEventListener('click', (e) => {
+    e.preventDefault()
+    const main = document.querySelector('main')
+    main.innerHTML = ''
+    main.innerHTML = '<h1>HOME</h1>'
+})
+
+characterInfo.addEventListener('click', (e) => {
+    e.preventDefault()
+    const main = document.querySelector('main')
+    main.innerHTML = ''
+    main.innerHTML = '<h1>Character Info</h1>'
+})
+function getCharacterInfo(){
+// fetch(api_url)
+// .then( (r) => r.json())
+// .then((data) => console.log(data))
+}
